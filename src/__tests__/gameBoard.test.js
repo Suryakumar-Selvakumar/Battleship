8ec2007b8f3f4c7ab8a02fc32173db073316f8b9
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-09-21 17:45:07 -0600

    feat(Gameboard): Place ships in proper indexes
    Add an additional check that ensures the ship is placed only when the
    indexes along its entire length are all empty and defined.

--- src/__tests__/gameBoard.test.js
+++ src/__tests__/gameBoard.test.js
@@ -11,49 +11,31 @@ describe("Gameboard", () => {
     }
   );
 
-  it.skip("allows to place ships at [0,0] coordinates on the board", () => {
-    gameBoard.placeShip(2, [0, 0], "left");
-    expect(gameBoard.board[0][0]).toEqual(expect.any(Ship));
-    expect(gameBoard.board[0][0]).toHaveProperty("length");
-    expect(gameBoard.board[0][0]).toHaveProperty("hits");
-    expect(gameBoard.board[0][0]).toHaveProperty("sunk");
-    expect(gameBoard.board[0][0]).toHaveProperty("placement");
-  });
-
-  it.skip("allows to place ship objects at [9,9] coordinates on the board", () => {
-    gameBoard.placeShip(3, [9, 9], "left");
-    expect(gameBoard.board[9][9]).toEqual(expect.any(Ship));
-    expect(gameBoard.board[9][9]).toHaveProperty("length");
-    expect(gameBoard.board[9][9]).toHaveProperty("hits");
-    expect(gameBoard.board[9][9]).toHaveProperty("sunk");
-    expect(gameBoard.board[9][9]).toHaveProperty("placement");
-  });
-
-  it("places ship(3, 'left') objects at adj. coordinates", () => {
+  it("places ship(3, [0, 9],'left') objects at adj. coordinates", () => {
     gameBoard.placeShip(3, [0, 9], "left");
     expect(gameBoard.board[0][8]).toEqual(expect.any(Ship));
     expect(gameBoard.board[0][7]).toEqual(expect.any(Ship));
   });
 
-  it("places ship(4, 'left') objects at adj. coordinates", () => {
+  it("places ship(4, [6, 7], 'left') objects at adj. coordinates", () => {
     gameBoard.placeShip(4, [6, 7], "left");
     expect(gameBoard.board[6][6]).toEqual(expect.any(Ship));
     expect(gameBoard.board[6][5]).toEqual(expect.any(Ship));
     expect(gameBoard.board[6][4]).toEqual(expect.any(Ship));
   });
 
-  it("places ship(3, 'right') objects at adj. coordinates", () => {
+  it("places ship(3, [1, 0], 'right') objects at adj. coordinates", () => {
     gameBoard.placeShip(3, [1, 0], "right");
     expect(gameBoard.board[1][1]).toEqual(expect.any(Ship));
     expect(gameBoard.board[1][2]).toEqual(expect.any(Ship));
   });
 
-  it("places ship(2, 'right') objects at adj. coordinates", () => {
+  it("places ship(2, [3, 0], 'right') objects at adj. coordinates", () => {
     gameBoard.placeShip(2, [3, 0], "right");
     expect(gameBoard.board[3][1]).toEqual(expect.any(Ship));
   });
 
-  it("places ship(4, 'top') objects at adj. coordinates", () => {
+  it("places ship(4, [5, 5], 'top') objects at adj. coordinates", () => {
     gameBoard.placeShip(4, [5, 5], "top");
     expect(gameBoard.board[4][5]).toEqual(expect.any(Ship));
     expect(gameBoard.board[3][5]).toEqual(expect.any(Ship));
@@ -61,19 +43,29 @@ describe("Gameboard", () => {
     // expect(gameBoard.placeShip()).toBe(1)
   });
 
-  it("places ship(2, 'top') objects at adj. coordinates", () => {
+  it("places ship(2, [9, 0], 'top') objects at adj. coordinates", () => {
     gameBoard.placeShip(2, [9, 0], "top");
     expect(gameBoard.board[8][0]).toEqual(expect.any(Ship));
   });
 
-  it("places ship(3, 'bottom') objects at adj. coordinates", () => {
+  it("places ship(3, [5, 9], 'bottom') objects at adj. coordinates", () => {
     gameBoard.placeShip(3, [5, 9], "bottom");
     expect(gameBoard.board[6][9]).toEqual(expect.any(Ship));
     expect(gameBoard.board[7][9]).toEqual(expect.any(Ship));
   });
 
-  it("places ship(2, 'right') objects at adj. coordinates", () => {
+  it("places ship(2, [7, 3], 'right') objects at adj. coordinates", () => {
     gameBoard.placeShip(2, [7, 3], "bottom");
     expect(gameBoard.board[8][3]).toEqual(expect.any(Ship));
+  });
+
+  it("refuses to place ships when adj. coordinates aren't available", () => {
+    gameBoard.placeShip(2, [0, 0], "left");
+    expect(gameBoard.board[0][0]).toEqual(expect.any(Ship));
+  });
+
+  it("refuses to place ships when adj. coordinates aren't available", () => {
+    gameBoard.placeShip(2, [9, 9], "right");
+    expect(gameBoard.board[0][0]).toEqual(expect.any(Ship));
   });
 });
--- src/gameBoard.js
+++ src/gameBoard.js
@@ -17,31 +17,45 @@ export class Gameboard {
   }
 
   placeShip(shipLength, coord, placement) {
+    let count = 0;
     for (let i = 0; i < shipLength; i++) {
       if (placement === "left") {
         if (
           this.board[coord[0]][coord[1] - i] === null &&
           this.board[coord[0]][coord[1] - i] !== undefined
         )
-          this.board[coord[0]][coord[1] - i] = new Ship(shipLength, placement);
+          count++;
       } else if (placement === "right") {
         if (
           this.board[coord[0]][coord[1] + i] === null &&
           this.board[coord[0]][coord[1] + i] !== undefined
         )
-          this.board[coord[0]][coord[1] + i] = new Ship(shipLength, placement);
+          count++;
       } else if (placement === "top") {
         if (
           this.board[coord[0] - i][coord[1]] === null &&
           this.board[coord[0] - i][coord[1]] !== undefined
         )
-          this.board[coord[0] - i][coord[1]] = new Ship(shipLength, placement);
+          count++;
       } else if (placement === "bottom") {
         if (
           this.board[coord[0] - i][coord[1]] === null &&
           this.board[coord[0] - i][coord[1]] !== undefined
         )
+          count++;
+      }
+    }
+    for (let i = 0; i < shipLength; i++) {
+      if (count === shipLength) {
+        if (placement === "left") {
+          this.board[coord[0]][coord[1] - i] = new Ship(shipLength, placement);
+        } else if (placement === "right") {
+          this.board[coord[0]][coord[1] + i] = new Ship(shipLength, placement);
+        } else if (placement === "top") {
+          this.board[coord[0] - i][coord[1]] = new Ship(shipLength, placement);
+        } else if (placement === "bottom") {
           this.board[coord[0] + i][coord[1]] = new Ship(shipLength, placement);
+        }
       }
     }
   }
