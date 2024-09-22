e52c5ccbdadbbae28519ff6c07dc9eff1e6855e9
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-09-22 15:23:13 -0600

    feat(DOM): Create 2 grids for human and computer
    Create 2 10x10 grids using the Gameboard object's board. One's for human
    and one is for the computer. Add basic styling to the grids and the page

--- src/helper.js
+++ src/helper.js
@@ -5,4 +5,23 @@ function randomize(placement) {
   return randomize(placement);
 }
 
-export { randomize };
+function createGrid(num) {
+  let mod = num + 1;
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    if (i % mod === 0) {
+      div.className = "row-divider";
+      div.style.width = "100%";
+      div.style.height = "0";
+      div.style.border = "0";
+    } else {
+      div.style.width = maxWidth / num + "px";
+      div.style.height = maxWidth / num + "px";
+      div.style.border = "solid 1px black";
+    }
+    container.appendChild(div);
+  }
+  updateEventListeners();
+}
+
+export { randomize, createGrid};
--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ import { Player } from "./player";
 import "./style.css";
 
 const mainContent = document.querySelector(".main-content");
-const locationForm = document.querySelector(".location-form");
+
+const human = new Player("human");
+const computer = new Player("computer");
+
+const humanBoard = document.querySelector(".player-grid");
+const computerBoard = document.querySelector(".computer-grid");
+
+for(let i = 0; i < human.gameBoard.board.length; i++) {
+    for(let j = 0; j < human.gameBoard.board[0].length; j++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-cell")
+        humanBoard.appendChild(div);
+    }
+}
+
+for(let i = 0; i < computer.gameBoard.board.length; i++) {
+    for(let j = 0; j < computer.gameBoard.board[0].length; j++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-cell")
+        computerBoard.appendChild(div);
+    }
+}
